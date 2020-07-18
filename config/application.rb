bbf0eb8dd289476c1bd68516238f1ca139184158
Meowyam <maryam@chilon.net>
2020-07-18 13:24:32 +0800

    still getting heroku to work

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,13 @@ module Grocerise
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'http://localhost:3000', 'http://grocerlist.herokuapp.com/' #replace this url with that of your own heroku client app
+        resource '*', :headers => :any, :methods => [:get]
+      end
+    end
+
   end
 end
